3f591731bf08e24a8266dd1a8c2ba7628e960fbc
Dawi Lu <dawi9840@gmail.com>
2024-02-16 10:17:15 +0800

    Update ClusterActivity.java

--- qt62JniValueToQML/android/src/com/foxconn/ClusterActivity.java
+++ qt62JniValueToQML/android/src/com/foxconn/ClusterActivity.java
@@ -7,15 +7,6 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 
-// import lookup CAN ID 相關庫
-import com.foxconn.util.SignalCANInfo;
-import com.foxconn.util.HexValueLookup;
-import com.foxconn.util.SpecificCanIdDataset;
-import com.foxconn.util.SignalReceiver;
-import java.util.Queue;
-
-import com.foxconn.QtAndroidService;
-
 public class ClusterActivity extends org.qtproject.qt.android.bindings.QtActivity {
     private static final String TAG = "CluserActivity";
 
@@ -23,14 +14,13 @@ public class ClusterActivity extends org.qtproject.qt.android.bindings.QtActivit
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Disable system bar visible.
-        getWindow().getDecorView().setSystemUiVisibility(
+        /*getWindow().getDecorView().setSystemUiVisibility(
         View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        //dawiTestClassCode();
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);*/
     }
 
     @Override
@@ -40,17 +30,5 @@ public class ClusterActivity extends org.qtproject.qt.android.bindings.QtActivit
             return true;
         }
         return super.onKeyDown(keyCode, event);
-    }
-
-    private void dawiTestClassCode(){
-        Log.d(TAG, "dawi_TestClassCode");
-        //SignalCANInfo.testSpecificIDsForHexValueStatus();
-        int count = 255;
-        int selectMode = 0; // [3]: Telltail and Vehicle status mode.
-        String inputSimulateCanSignals = SignalReceiver.simulateCanSignals(count);
-        Queue<String> tmpSignalQueue = SignalReceiver.getSignalQueue(inputSimulateCanSignals);
-        String aString = SignalReceiver.getQueueToString(tmpSignalQueue);
-        Log.d(TAG, "aString: " + aString);
-        SignalCANInfo.findSpecificIDsForHexValueStatus(aString, selectMode);
     }
 }
